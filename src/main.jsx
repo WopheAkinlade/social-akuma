f341035186d0aca3c38d22b1f1ca807284f60ac4
Wophe <wophe.akinlade@gmail.com>
2023-07-06 17:14:30 +0100

    Starting from scratch lmao

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Sidebar from "./routes/root";
-import Post from "./routes/Post";
+// import Sidebar from "./routes/root";
 import Error from "./routes/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Sidebar />,
+    // element: <Sidebar />,
     errorElement: <Error />
-    // children: {
-    //   path: "/",
-    //   element: <Post />
-    // }
   },
 ]);
 
