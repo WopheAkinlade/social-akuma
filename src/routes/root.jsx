538733dfcbf49381ee87a5cc302810e2339aea3a
Wophe <wophe.akinlade@gmail.com>
2023-07-18 20:52:50 +0100

    Done with the styling of the sidebar

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,40 +1,93 @@
-import { Home } from "@mui/icons-material";
+import {
+  Email,
+  Explore,
+  Group,
+  Home,
+  Person,
+  Settings,
+} from "@mui/icons-material";
 import {
   Box,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
-  ListItemText,
+  Typography,
 } from "@mui/material";
 // For testing purposes: height={300} width={300} bgcolor={"red"}
 
 const mainStyle = {
-  paddingTop: 5,
+  // position: "fixed",
   height: "100vh",
   width: "20rem",
   display: "flex",
   flexDirection: "column",
-  justiyContent:"center",
+  justiyContent: "center",
   alignItems: "center",
-}
+  borderRight: "1.7px solid"
+};
+
+const iconStyle = {
+  color: "black",
+  height: "2rem",
+  width: "auto",
+};
 
 const Sidebar = () => {
-
   return (
     <Box sx={mainStyle}>
       <div className="logo-box">
         <h1 className="logo-text">Social Things</h1>
       </div>
 
-      <Box width={"80%"} mt={"0.5rem"}>
+      <Box width={"80%"}>
         <List>
-          <ListItem disablePadding>
+          <ListItem sx={{ marginBottom: 5 }} disablePadding>
             <ListItemButton component="a" href="#home">
               <ListItemIcon>
-                <Home />
+                <Home sx={iconStyle} />
               </ListItemIcon>
-              <ListItemText primary="Homepage" />
+              <Typography fontSize={20} fontWeight={500}>Homepage</Typography>
+            </ListItemButton>
+          </ListItem>
+          <ListItem sx={{ marginBottom: 5 }} disablePadding>
+            <ListItemButton component="a" href="#account">
+              <ListItemIcon>
+                <Person sx={iconStyle} />
+              </ListItemIcon>
+              <Typography fontSize={20} fontWeight={500}>Profile</Typography>
+            </ListItemButton>
+          </ListItem>
+          <ListItem sx={{ marginBottom: 5 }} disablePadding>
+            <ListItemButton component="a" href="#friends">
+              <ListItemIcon>
+                <Group sx={iconStyle} />
+              </ListItemIcon>
+              <Typography fontSize={20} fontWeight={500}>Friends</Typography>
+            </ListItemButton>
+          </ListItem>
+          <ListItem sx={{ marginBottom: 5 }} disablePadding>
+            <ListItemButton component="a" href="#explore">
+              <ListItemIcon>
+                <Explore sx={iconStyle} />
+              </ListItemIcon>
+              <Typography fontSize={20} fontWeight={500}>Explore</Typography>
+            </ListItemButton>
+          </ListItem>
+          <ListItem sx={{ marginBottom: 5 }} disablePadding>
+            <ListItemButton component="a" href="#settings">
+              <ListItemIcon>
+                <Settings sx={iconStyle} />
+              </ListItemIcon>
+              <Typography fontSize={20} fontWeight={500}>Settings</Typography>
+            </ListItemButton>
+          </ListItem>
+          <ListItem sx={{ marginBottom: 5 }} disablePadding>
+            <ListItemButton component="a" href="#messages">
+              <ListItemIcon>
+                <Email sx={iconStyle} />
+              </ListItemIcon>
+              <Typography fontSize={20} fontWeight={500}>Messages</Typography>
             </ListItemButton>
           </ListItem>
         </List>
